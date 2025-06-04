e79a9de0495eb5921a2dfedfb92e9131d3a23c91
Diyor18 <dixon.munoz@ucr.ac.cr>
2025-06-03 18:26:55 -0600

    subiendo pruebas

---
+++ prueba.test.js
@@ -0,0 +1,12 @@
+// calculadora.test.js
+const { sumar, dividir } = require('./prueba');
+
+test('suma 2 + 2 debe dar 4', () => {
+  expect(sumar(2, 2)).toBe(4);
+});
+
+
+test('la division de 10 entre 5 debe dar 2', () => {
+expect(dividir(10,5)).toBe(2);
+
+})
